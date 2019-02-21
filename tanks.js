060e55d1209b389902db53222ea48b4d4ef36b87
Toksaitov Dmitrii Alexandrovich <dmitrii@toksaitov.com>
2019-02-21 13:56:21 +0000

    Closes #3, closes #4
    Реализовать код ввода с клавиатуры для управления танком
    Реализовать код ввода с клавиатуры для управления танком

--- tanks.js
+++ tanks.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
 
 const port = 8080
 
 app.use(express.static('public'))
 
-app.listen(port, () => {
+io.on('connection', socket => {
+    socket.on('cmd', key => console.log(key))
+})
+
+http.listen(port, () => {
     console.log(`The Tanks server is listening on port ${port}`)
 })
